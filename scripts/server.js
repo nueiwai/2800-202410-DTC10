4b53844188684602b547e76e646868b5e230af58
Michael <michael.lau@hotmail.ca>
2024-05-14 23:11:09 -0700

    Add comments to functions and rearrange routes

--- scripts/server.js
+++ scripts/server.js
@@ -7,7 +7,6 @@ const ejs = require('ejs');
 const userModel = require('./users')
 const MongoStore = require('connect-mongo');
 const cors = require('cors')
-
 require('dotenv').config();
 
 /* secret information section */
@@ -24,7 +23,7 @@ const db = MongoStore.create({
   mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}`
 })
 
-// Express application setup
+// Application configuration
 app.set('view engine', 'ejs');
 app.use(cors())
 app.use(express.json());
@@ -41,19 +40,38 @@ app.use(session({
   cookie: { secure: false, maxAge: 3600000 }
 }))
 
+// Start application
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+// Landing page route
 app.get('/', async (req, res) => {
   res.render("index")
-  console.log(req.session.userid)
 })
 
+// Signup route
 app.get('/signup', (req, res) => {
   res.render("signup")
 })
 
+// Login route
+app.get('/login', (req, res) => {
+  res.render("login")
+})
+
+// Post login route
+app.get('/postlogin', (req, res) => {
+  res.render("postlogin")
+})
+
+// Logout route
+app.post('/logout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
+})
+
+// Creates a new user from the body of the submitted form and writes to database
 app.post('/signup', async (req, res) => {
   const newUser = await new userModel({
     name: req.body.name,
@@ -65,13 +83,11 @@ app.post('/signup', async (req, res) => {
   res.redirect("postlogin")
 })
 
-app.get('/login', (req, res) => {
-  res.render("login")
-})
-
+// Checks the login information and redirects to landing page if successful, otherwise redirect to index 
 app.post('/login', async (req, res) => {
   const user = await userModel.findOne({email: req.body.email})
   const isAuth = await bcrypt.compare(req.body.password, user.password)
+
   if (isAuth){
     req.session.userid = user.id
     return res.redirect("postlogin")
@@ -81,14 +97,22 @@ app.post('/login', async (req, res) => {
   }
 })
 
-app.get('/postlogin', (req, res) => {
-  res.render("postlogin")
+// Returns user info of session owner
+app.get('/getInfo', async (req, res) => {
+  const userID = req.session.userid
+  const userInfo = await userModel.findById({_id: userID})
+  // console.log(userInfo)
+  res.send(userInfo)
 })
 
-app.post('/logout', (req, res) => {
-  req.session.destroy()
-  res.redirect('/')
+// Updates the name of user
+app.post('/update', async(req, res) => {
+  const user = await userModel.findById(req.body.userID)
+  user.name = req.body.userName
+  await user.save()
+  console.log("name saved")
 })
+
 
 
 
