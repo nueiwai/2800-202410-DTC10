6061166b95f2d79c8abffba0ddf2dc693555d8ed
Michael <michael.lau@hotmail.ca>
2024-05-15 17:32:48 -0700

    Link ejs components together to create a post login page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
 const session = require('express-session')
 const bcrypt = require('bcrypt')
 const ejs = require('ejs');
