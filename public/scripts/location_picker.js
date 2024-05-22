1dc42afe7a5425811caf41559d8f43fff0408715
Nu Ei Wai <nueiwai724665@gmail.com>
2024-05-22 14:05:00 -0700

    Make the "Your location" text pop up in the search bar when get current location button is clicked

--- public/scripts/location_picker.js
+++ public/scripts/location_picker.js
@@ -1,81 +1,43 @@
-
 function getCurrentLocationStart() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPositionStart);
   } else {
     alert("Geolocation is not supported by this browser.");
   }
 }
 
-function showPosition(position) {
+function showPositionStart(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   console.log(latitude)
   console.log(longitude)
   $.post('getAddress', { lat: latitude, lng: longitude }, function (data) {
     let address = data.features[0].properties.full_address
+    document.querySelector('.start-geocoder>.mapboxgl-ctrl-geocoder>.mapboxgl-ctrl-geocoder--input').setAttribute('value', 'Your Location');
     console.log(address)
-    document.getElementById("starting-location").value = address
   }).fail(function () {
     console.log("error")
   })
 }
-
 
 function getCurrentLocationEnd() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPositionEnd);
   } else {
     alert("Geolocation is not supported by this browser.");
   }
 }
 
-function showPosition(position) {
+function showPositionEnd(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   console.log(latitude)
   console.log(longitude)
   $.post('getAddress', { lat: latitude, lng: longitude }, function (data) {
     let address = data.features[0].properties.full_address
+    document.querySelector('.end-geocoder>.mapboxgl-ctrl-geocoder>.mapboxgl-ctrl-geocoder--input').setAttribute('value', 'Your Location');
     console.log(address)
-    document.getElementById("destination").value = address
   }).fail(function () {
     console.log("error")
   })
 }
-
-
-// function getCurrentLocationEnd() {
-//   if ("geolocation" in navigator) {
-//     let options = {
-//       enableHighAccuracy: true,
-//       timeout: 5000,
-//       maximumAge: 0
-//     };
-
-//     let successCallback = (position) => {
-//       let latitude = position.coords.latitude;
-//       let longitude = position.coords.longitude;
-//       let accuracy = position.coords.accuracy;
-//       let timestamp = position.timestamp;
-
-//       console.log(`Latitude: ${latitude}, Longitude: ${longitude}, Accuracy: ${accuracy} meters (Timestamp: ${new Date(timestamp)})`);
-//       $.post('getAddress', { lat: latitude, lng: longitude }, function (data) {
-//         console.log(data)
-//       }).fail(function () {
-//         console.log("error")
-//       })
-//     };
-
-//     let errorCallback = (error) => {
-//       console.error(`Error getting location: ${error.code} - ${error.message}`);
-//     };
-
-//     let watchId = navigator.geolocation.watchPosition(successCallback, errorCallback, options);
-
-//     // Store the watchId to stop watching later if needed
-//     // navigator.geolocation.clearWatch(watchId);
-//   } else {
-//     console.error("Geolocation is not supported by this browser.");
-//   }
-// }
