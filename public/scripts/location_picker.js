a26456f7118728d0d569c883c4b622f48ee88bc5
Nu Ei Wai <nueiwai724665@gmail.com>
2024-05-23 01:15:44 -0700

    Write code to make the get current location and routing work together

--- public/scripts/location_picker.js
+++ public/scripts/location_picker.js
@@ -1,3 +1,9 @@
+/**
+ * Function to check if the browser supports geolocation and get the current position.
+ * Calls showPositionStart if the geolocation is supported.
+ * Alerts the user if the geolocation is not supported.
+ * @returns {void}
+ */
 function getCurrentLocationStart() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPositionStart);
@@ -6,16 +12,28 @@ function getCurrentLocationStart() {
   }
 }
 
+
+
+/**
+ * Function to get the current location of the user for starting location
+ * @returns {void}
+ */
 function showPositionStart(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   console.log(latitude)
   console.log(longitude)
   let currentStartLocation = [longitude, latitude]
-  sessionStorage.setItem('currentStartLocation', JSON.stringify(currentStartLocation));
+  document.getElementById('current-starting-location').setAttribute('value', currentStartLocation);
   document.querySelector('.start-geocoder>.mapboxgl-ctrl-geocoder>.mapboxgl-ctrl-geocoder--input').setAttribute('value', 'Your Location');
 }
 
+/**
+ * Function to check if the browser supports geolocation and get the current position.
+ * Calls showPositionEnd if the geolocation is supported.
+ * Alerts the user if the geolocation is not supported.
+ * @returns {void}
+ */
 function getCurrentLocationEnd() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPositionEnd);
@@ -24,12 +42,16 @@ function getCurrentLocationEnd() {
   }
 }
 
+/**
+ * Function to get the current location of the user for ending location
+ * @returns {void}
+ */
 function showPositionEnd(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   console.log(latitude)
   console.log(longitude)
   let currentEndLocation = [longitude, latitude]
-  sessionStorage.setItem('currentEndLocation', JSON.stringify(currentEndLocation));
+  document.getElementById('current-destination').setAttribute('value', currentEndLocation);
   document.querySelector('.end-geocoder>.mapboxgl-ctrl-geocoder>.mapboxgl-ctrl-geocoder--input').setAttribute('value', 'Your Location');
 }
