220dbd23c92152ebc24ad932ee172a9591fcdfa8
Nu Ei Wai <nueiwai724665@gmail.com>
2024-05-27 07:58:25 -0700

    connect the components to session storage

--- public/script.js
+++ public/script.js
@@ -1,37 +1,33 @@
 let directDeliveryBtn = $("#directDeliveryBtn")
-let locationModal = $("#delivery-location-modal")
-let locationModalCloseBtn = $("#location-modal-close-btn")
+let roadsideAssistanceBtn = $("#roadsideAssistanceBtn")
+let droneShareBtn = $("#droneShareBtn")
+let locationModal = $("#deliveryLocationModal")
 let confirmLocationBtn = $("#confirmBtn")
 let mainMenuCard = $("#mainMenuCard")
-let sizeSelectMenu = $("#selectSizeMenu")
+let packageSizeOptions = $("#packageSizeOptions")
 let selectSizeNextBtn = $("#selectSizeNextBtn")
-let paymentMethodContainer = $("#paymentMethodContainer")
+let paymentMethods = $("#paymentMethods")
 let paymentMethodNextBtn = $("#paymentMethodNextBtn")
-let confirmationMenuContainer = $("#confirmationMenuContainer")
+let confirmationMenuContainer = $("#confirmationCard")
 
 directDeliveryBtn.click(() => {
     console.log("move location modal down")
-    locationModal.removeClass("transform -translate-y-full")
-    locationModal.addClass("transition-transform translate-y-[80px]")
+    sessionStorage.setItem("feature", "direct")
 })
 
-locationModalCloseBtn.click(() => {
-    locationModal.removeClass("transform translate-y-[80px]")
-    locationModal.addClass("transition-transform -translate-y-full")
-});
-
+droneShareBtn.click(() => {
+    console.log("move location modal down")
+    sessionStorage.setItem("feature", "share")
+})
 
 // Step 2: When user confirms location in #delivery-location-modal, animate transition to #sizeSelectMenu
 // ToDo: Adjust speed of transition
 confirmLocationBtn.click(() => {
-    // Move location modal back up
-    locationModal.addClass("transition-transform -translate-y-full")
-
     // Remove mainMenuCard 
-    mainMenuCard.addClass("transition-transform -translate-x-full")
+    mainMenuCard.addClass("transition-transform translate-y-full")
 
     // Show sizeSelectMenu
-    sizeSelectMenu.toggle()
+    packageSizeOptions.toggle()
 
     // Hide mainMenuCard
     mainMenuCard.toggle()
@@ -39,25 +35,26 @@ confirmLocationBtn.click(() => {
 
 // Step 3: When user clicks next on #sizeSelectMenu, animate and transition to #paymentMethodContainer
 selectSizeNextBtn.click(() => {
-    const elementHeight = paymentMethodContainer.outerHeight() + 60
-    paymentMethodContainer.height(elementHeight)
-    sizeSelectMenu.removeClass("transform translate-x-full transition-transform bottom-0 left-0 right-0 transform-none")
-    sizeSelectMenu.addClass("transition-transform -translate-x-full")
+    // const elementHeight = paymentMethods.outerHeight() + 60
+    // paymentMethods.height(elementHeight)
+    packageSizeOptions.addClass("transition-transform -translate-x-full")
 
     setTimeout(() => {
-        paymentMethodContainer.removeClass("transform translate-x-full")
-        paymentMethodContainer.addClass("transition-transform -translate-x-0")
+        // paymentMethods.addClass("transform translate-x-full")
+        paymentMethods.toggle()
     }, 150);
+
+    packageSizeOptions.toggle()
 })
 
 
 paymentMethodNextBtn.click(() => {
-    paymentMethodContainer.addClass("transition-none")
-    paymentMethodContainer.removeClass("transition-none")
-    paymentMethodContainer.addClass("transition-transform -translate-x-full")
+    paymentMethods.addClass("transition-none")
+    paymentMethods.removeClass("transition-none")
+    paymentMethods.addClass("transition-transform -translate-x-full")
 
     setTimeout(() => {
-        paymentMethodContainer.toggle("hidden")
+        paymentMethods.toggle("hidden")
         confirmationMenuContainer.removeClass("hidden")
         confirmationMenuContainer.addClass("transform translate-x-full")
         confirmationMenuContainer.removeClass("transform translate-x-full")
