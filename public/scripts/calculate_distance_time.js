6d4a740c519fd11485c324832495e0c8ee060738
Nu Ei Wai <nueiwai724665@gmail.com>
2024-05-27 16:44:31 -0700

    fix the layout of confirmation section in the modal

--- public/scripts/calculate_distance_time.js
+++ public/scripts/calculate_distance_time.js
@@ -1,3 +1,9 @@
+/**
+ * calculate straight line distance between two points and store it in session storage
+ * @param {Array} start 
+ * @param {Array} end 
+ * @returns {void} 
+ */
 async function calculateDistance(start, end) {
   console.log(start, end);
   var from = turf.point(start);
@@ -6,12 +12,14 @@ async function calculateDistance(start, end) {
   sessionStorage.setItem("distance", distance);
   calculatePrices(distance);
   calculateTime();
-  return distance;
 }
 
+/**
+ * calculate prices based on distance and store it in session storage
+ * @returns {void}
+ */
 async function calculateTime() {
   var distance = sessionStorage.getItem("distance");
   var time = distance / (55 / 60);
   sessionStorage.setItem("estimatedDuration", time);
-  return time;
 }
