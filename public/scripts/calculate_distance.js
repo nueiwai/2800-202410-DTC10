40a2f63e19c8cb945d5ef4449fd80e66b1517cfc
Nu Ei Wai <nueiwai724665@gmail.com>
2024-05-27 09:45:50 -0700

    delete erronous line that is commented out

--- public/scripts/calculate_distance.js
+++ public/scripts/calculate_distance.js
@@ -2,7 +2,6 @@ async function calculateDistance(start, end) {
   console.log(start, end);
   var from = turf.point(start);
   var to = turf.point(end);
-  // var options = { units: 'kilometers' };
   var distance = turf.distance(from, to);
   sessionStorage.setItem("distance", distance);
   calculatePrices(distance);
